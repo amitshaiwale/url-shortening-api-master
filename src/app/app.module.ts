0a18916d7d1295715dea3461854a4179af1786b9
Amit Shaiwale <amitshaiwale20@gmail.com>
2020-04-16 01:31:34 +0530

    initial UI development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { UrlSectionComponent } from './url-section/url-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GetStartedComponent,
+    UrlSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/url-section/url-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-url-section',
+  templateUrl: './url-section.component.html',
+  styleUrls: ['./url-section.component.css']
+})
+export class UrlSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
